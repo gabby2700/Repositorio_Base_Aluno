6dc6eee9c0e7e75b81921b4f1bd2f5af618def9d
gabby2700 <ferreiragabrielly844@gmail.com>
2026-05-12 08:41:39 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/luncoes.py
@@ -0,0 +1,16 @@
+#Declaçao de funcao
+
+def soma(n1,n2) :
+    return n1 + n2
+
+print(soma(6,2))
+soma(3,3)
+ 
+
+#Facam uma funçao que receba 2 parametros e multiplique esses dois valores
+
+
+def multicacao(n1,n2) :
+    return n1 + n2
+
+print(multicacao(7,8))
